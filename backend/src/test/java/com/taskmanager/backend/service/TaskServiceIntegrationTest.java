ec0830b992b8a36b699c4c744a1c00e3ac62ac29
Philip Domann <philip.domann@specific-group.com>
2023-12-21 17:42:13 -0600

    testOptimisticLocking

---
+++ backend/src/test/java/com/taskmanager/backend/service/TaskServiceIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.taskmanager.backend.service;
+
+import com.taskmanager.backend.model.Task;
+import com.taskmanager.backend.repository.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class TaskServiceIntegrationTest {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Test
+    void testOptimisticLocking() {
+        Task task = Task.builder().name("Initial Task").priority(Task.Priority.MEDIUM).build();
+        task = taskRepository.save(task);
+
+        Task taskTx1 = taskRepository.findById(task.getId())
+                .orElseThrow(() -> new AssertionError("Task not found"));
+        Task taskTx2 = taskRepository.findById(task.getId())
+                .orElseThrow(() -> new AssertionError("Task not found"));
+
+        taskTx1.setName("Updated in Tx1");
+        taskRepository.save(taskTx1);
+
+        taskTx2.setName("Updated in Tx2");
+        assertThrows(ObjectOptimisticLockingFailureException.class, () -> {
+            taskRepository.save(taskTx2);
+        });
+    }
+}
+
