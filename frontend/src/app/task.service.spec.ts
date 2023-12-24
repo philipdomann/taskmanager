88eccfc37b5b7e3fb6293898c3acc0944cb6b681
Philip Domann <philip.domann@specific-group.com>
2023-12-23 19:25:11 -0600

    taskservice and tests

---
+++ frontend/src/app/task.service.spec.ts
@@ -0,0 +1,78 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TaskService, Task } from './task.service';
+
+describe('TaskService', () => {
+  let service: TaskService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TaskService]
+    });
+
+    service = TestBed.inject(TaskService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrieve all tasks', () => {
+    const mockTasks: Task[] = [
+      { id: 1, name: 'Test Task 1', done: false, created: new Date(), priority: 'LOW' },
+      { id: 2, name: 'Test Task 2', done: false, created: new Date(), priority: 'URGENT' }
+    ];
+
+    service.getTasks().subscribe(tasks => {
+      expect(tasks.length).toBe(2);
+      expect(tasks).toEqual(mockTasks);
+    });
+
+    const req = httpTestingController.expectOne(service.apiUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockTasks);
+  });
+
+  it('should add a new task', () => {
+    const newTask: Task = { name: 'New Task', done: false, created: new Date(), priority: 'NORMAL' };
+
+    service.addTask(newTask).subscribe(task => {
+      expect(task).toEqual(jasmine.objectContaining(newTask));
+    });
+
+    const req = httpTestingController.expectOne(service.apiUrl);
+    expect(req.request.method).toEqual('POST');
+    req.flush({ ...newTask, id: 1 });
+  });
+
+  it('should remove a task', () => {
+    const taskId = 1;
+
+    service.removeTask(taskId).subscribe(response => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpTestingController.expectOne(`${service.apiUrl}/${taskId}`);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush(null);
+  });
+
+  it('should edit a task', () => {
+    const updatedTask: Task = { id: 1, name: 'Updated Task', done: true, created: new Date(), priority: 'URGENT' };
+
+    service.editTask(updatedTask).subscribe(task => {
+      expect(task).toEqual(updatedTask);
+    });
+
+    const req = httpTestingController.expectOne(`${service.apiUrl}/${updatedTask.id}`);
+    expect(req.request.method).toEqual('PUT');
+    req.flush(updatedTask);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+});
---
+++ frontend/src/app/task.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Task {
+  id?: number;
+  name: string;
+  done: boolean;
+  created: Date;
+  priority: 'LOW' | 'NORMAL' | 'URGENT';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private _apiUrl = 'http://your-api-url/api/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this._apiUrl);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this._apiUrl, task);
+  }
+
+  removeTask(taskId: number): Observable<void> {
+    return this.http.delete<void>(`${this._apiUrl}/${taskId}`);
+  }
+
+  editTask(task: Task): Observable<Task> {
+    return this.http.put<Task>(`${this._apiUrl}/${task.id}`, task);
+  }
+
+  get apiUrl(): string {
+    return this._apiUrl;
+  }
+}
