bd953513164b74564906929dff90af7e70d25127
Philip Domann <philip.domann@specific-group.com>
2023-12-28 07:32:49 -0600

    v0.0.1

--- frontend/src/app/interceptors/error.interceptor.ts
+++ frontend/src/app/interceptors/error.interceptor.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpInterceptor, HttpRequest, HttpHandler, HttpEvent} from '@angular/common/http';
-import {catchError, tap} from 'rxjs/operators';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 import { ErrorHandlerService } from "../services/error-handler.service";
-import {Observable} from "rxjs";
-import {AlertService} from "../services/alert.service";
+import { Observable } from "rxjs";
+import { AlertService } from "../services/alert.service";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(private errorHandlerService: ErrorHandlerService, private alertService: AlertService) {}
+    constructor(private errorHandlerService: ErrorHandlerService, private alertService: AlertService) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Outgoing HTTP request', req);
-    return next.handle(req).pipe(
-      tap((event: HttpEvent<any>) => {
-        console.log('Incoming HTTP response', event);
-      }),
-      catchError(error => {
-          this.alertService.showError("Server is not reachable.");
-          return this.errorHandlerService.handleError(error);
-      })
-    );
-  }
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        console.log('Outgoing HTTP request', req);
+        return next.handle(req).pipe(
+            tap((event: HttpEvent<any>) => {
+                console.log('Incoming HTTP response', event);
+            }),
+            catchError((error: HttpErrorResponse) => {
+                if (error.status === 500) {
+                    this.alertService.showError("Server error happened.");
+                } else if (error.status === 400) {
+                    this.alertService.showError("Saving task failed.");
+                } else {
+                    this.alertService.showError("An unexpected error occurred.");
+                }
+                return this.errorHandlerService.handleError(error);
+            })
+        );
+    }
 }
